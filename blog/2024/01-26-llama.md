e07f0678f5c8cf880c4e16b3d46b7a52aa8a871b
kcxain <kcxain@gmail.com>
2024-02-03 10:50:05 +0800

    update

---
+++ blog/2024/01-26-llama.md
@@ -0,0 +1,10 @@
+---
+authors: kcxain
+unlisted: true
+tags: [NLP, 源码]
+---
+
+# LLaMA代码解读
+
+代码地址：[transformers/src/transformers/models/llama/modeling_llama.py at main · huggingface/transformers (github.com)](https://github.com/huggingface/transformers/blob/main/src/transformers/models/llama/modeling_llama.py)
+
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   favicon: 'img/favicon.svg',
 
   // Set the production url of your site here
-  url: 'https://deconx.cn',
+  url: 'https://kkee.top',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
